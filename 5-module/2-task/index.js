246f3891a183b26f846e70307c421d79667ffcfc
Vladislav Sushkevich <vladislav2908@gmail.com>
2022-05-14 21:27:23 +0300

    Little changes

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,7 +1,7 @@
 function toggleText() {
   const button = document.querySelector(".toggle-text-button");
   button.onclick = function () {
-    const text = document.getElementById("text");
+    let text = document.getElementById("text");
     text.hidden = !text.hidden;
   };
 }
