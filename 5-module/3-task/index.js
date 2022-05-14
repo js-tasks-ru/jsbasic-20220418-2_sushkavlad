2f1b605856858c6cc6ad3a6f1e65f24642c42ea1
Vladislav Sushkevich <vladislav2908@gmail.com>
2022-05-14 21:24:06 +0300

    Module 5 has been finished.

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,31 @@
 function initCarousel() {
-  // ваш код...
+  const rightArrow = document.querySelector(".carousel__arrow_right");
+  const leftArrow = document.querySelector(".carousel__arrow_left");
+  const carousel = document.querySelector(".carousel__inner");
+  const numOfSlides = 4;
+  let currentSlide = 0;
+
+  hideArrows();
+
+  leftArrow.addEventListener("click", () => {
+    currentSlide--;
+    slideShift();
+    hideArrows();
+  });
+
+  rightArrow.addEventListener("click", () => {
+    currentSlide++;
+    slideShift();
+    hideArrows();
+  });
+
+  function slideShift() {
+    let shift = -carousel.offsetWidth * currentSlide;
+    carousel.style.transform = `translateX(${shift}px)`;
+  }
+
+  function hideArrows() {
+    leftArrow.style.display = currentSlide === 0 ? "none" : "";
+    rightArrow.style.display = currentSlide === numOfSlides - 1 ? "none" : "";
+  }
 }
